9be752a420e5d1b563283468658581230a5b7434
nessco1234 <app1.nessco@gmail.com>
2025-01-15 11:00:07 +0530

    login using dummy user and password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import RecipeCreator from './components/RecipeCreator';
+import Favorites from './components/Favorites';
+import LoginRegister from './components/LoginRegister';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginRegister />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<RecipeCreator />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Router>
+    // <>
+    // <h1>App Component</h1>
+    // </>
   );
 }
 
